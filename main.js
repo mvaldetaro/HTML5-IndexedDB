9a884db13fa77568904b6da5a2c2f7323da029fb
mvaldetaro <mvaldetaro@gmail.com>
2014-06-10 11:24:16 -0300

    BD aberto

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 //Namespace
-var db = {};
-db.indexedDB = {};
+var mydb = {};
+mydb.indexedDB = {};
 
+// Abrir o banco de dados
+mydb.indexedDB = null;
+
+mydb.indexedDB.open = function() {
+  var request = indexedDB.open("todos");
+
+  request.onsuccess = function(e) {
+    mydb.indexedDB.db = e.target.result;
+  };
+
+  request.onfailure = mydb.indexedDB.onerror;
+}
