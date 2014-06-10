dfb3967d71f28c153b04427f121b146b7ee8b004
mvaldetaro <mvaldetaro@gmail.com>
2014-06-10 12:01:59 -0300

    Criando armazenamento de objetos

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 //Namespace
-var mydb = {};
-mydb.indexedDB = {};
+var estudo = {};
 
-// Abrir o banco de dados
-mydb.indexedDB = null;
+estudo.indexedDB = {};
+estudo.indexedDB.db = null;
 
-mydb.indexedDB.open = function() {
-  var request = indexedDB.open("todos");
+// Abrindo BD e criando armazenamento de objetos
 
+// Abre o BD
+estudo.indexedDB.open = function() {
+  var request = indexedDB.open("todos", "Descrição do banco de dados");
+  // "todos" é o nome do banco de dados
   request.onsuccess = function(e) {
-    mydb.indexedDB.db = e.target.result;
+    var v = "1.0";
+    estudo.indexedDB.db = e.target.result;
+    var db = estudo.indexedDB.db;
+
+    //Verifica a versão
+    if (v!= db.version) {
+      var setVrequest = db.version(v);
+
+      setVrequest.on.failure = estudo.indexedDB.onerror;
+      setVrequest.onsuccess = function(e) {
+        //Cria o armazenamento de objetos
+        var store = db.createObjectStore("todo", {keyPath: "timeStamp"});
+
+        estudo.indexedDB.getAllTodoItems();
+      };
+    }
+    estudo.indexedDB.getAllTodoItems();
   };
 
-  request.onfailure = mydb.indexedDB.onerror;
-}
+  request.onfailure = estudo.indexedDB.onerror;
+};
